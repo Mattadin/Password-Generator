9493655c2ad05661b0ec645900854b8d8ba7e365
Mattadin <m4tt4d1n@gmail.com>
2022-03-10 18:31:40 +1100

    Final commit, readMe done and code cleaned.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Password-Generator
+
+## The Project
+
+This project is designed to create a password generator using logical loops between particular criteria chosen by a user. It functions by creating prompts to collect user's information for the design of their custom password allowing some customizability to meet user needs. Comments left for ease of refactor or re-use for any other future projects of similar builds.
+
+## Usage
+
+Simply click the generate password button, answer a few simple questions and voila! Simple, straight forward, effective. My three favorite things!
+
+
+![screenshot](./assets/images/PasswordGenExample.PNG)
+
+## Credits
+
+Char codes guide: http:///www.net-comber.com/charset.html
+
+Special thanks to Leo Wong and Christi Scappatura for help reviewing and discussing clean and easy loops.
+
+## Features
+
+**Prompts for user choice of parameters for password.**
+**Selection of choices between password length, symbols (special characters), upper case, lower case, and numbers.**
+**Pre-emptive prompt to return users if their password length isn't 8 characters minimum or if text is used as input instead of numbers**
--- script.js
+++ script.js
@@ -6,22 +6,30 @@ function writePassword() {
     var passwordText = document.querySelector('#password');
     passwordText.value = password;
 }
+
+// Crude but effective method of ensuring users are unable to select a value not in target range
 var options = ['8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '100', '101', '102', '103', '104', '105', '106', '107', '108', '109', '110', '111', '112', '113', '114', '115', '116', '117', '118', '119', '120', '121', '122', '123', '124', '125', '126', '127', '128'];
 function generatePassword() {
     var userChoice = prompt('Enter number of characters desired (8-100)');
+    // Prevents users from "accidentally" choosing an option out of the target range
     if (!options.includes(userChoice)) {
         alert('The option selected ' + userChoice + ' is not a number between 8-128.');
         return;
     }
+
+    // Allows the function to read the user's input which is generally a "string" as a number instead for math functions.
     var userChoiceValue = parseInt(userChoice);
-    var minimumAmount = 0;
+
+    // Allows the function to read user's options as a true/false statement to simplify password generation
+
     var lowerCaseQuery = confirm('Allow use of lower case letters?');
     var upperCaseQuery = confirm('Allow use of upper case letters?');
     var symbolsQuery = confirm('Allow use of symbols?');
     var numbersQuery = confirm('Allow use of numbers?');
+
     // RNG Machine that pulls from char-codes
     // Char codes guide: http:///www.net-comber.com/charset.html
-    // Symbols was impractical, using local string instead.
+
     const randomFunc = {
         getRandomLower: function () {
             return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -29,6 +37,7 @@ function generatePassword() {
         getRandomUpper: function () {
             return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
         },
+           // Symbols were impractical, using local string instead.
         getRandomSymbol: function () {
             const symbols = '!@#$%^&*(){}[]=<>/,.';
             return symbols[Math.floor(Math.random() * symbols.length)];
@@ -38,6 +47,7 @@ function generatePassword() {
         },
     };
     var randomPasswordGenerated = '';
+    // where the magic happens. Looping to randomly generate a character for each slot of the user's choice length.
     console.log(`userChoiceValue(${userChoiceValue})`);
     for (let i = 0; i < userChoiceValue; i++) {
         var randomCharactersChosen = '';
